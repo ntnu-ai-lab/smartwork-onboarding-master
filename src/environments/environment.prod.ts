e30c7dee65a42d8b7933f645ca95c9344cb6d9a7
anuja13 <anujagit@gmail.com>
2025-01-29 12:13:32 +0100

    dev and prod endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   backend: {
     endpoints:{
-      eligibility: "https://smartwork.idi.ntnu.no/onboarding/eligibility",
-      register: "https://smartwork.idi.ntnu.no/onboarding/patient",
-      status: "https://smartwork.idi.ntnu.no/sendFromStatus/"
+      eligibility: "https://smartwork-onboarding.idi.ntnu.no/onboarding/eligibility",
+      register: "https://smartwork-onboarding.idi.ntnu.no/onboarding/patient",
+      status: "https://smartwork-onboarding.idi.ntnu.no/sendFromStatus/"
      
     }
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   backend: {
     endpoints:{
-      eligibility: "http://smartwork-dev.idi.ntnu.no:8013/onboarding/eligibility",
-      register: "http://smartwork-dev.idi.ntnu.no:8013/onboarding/patient",
-      status: "http://smartwork-dev.idi.ntnu.no:8013/onboarding/sendFromStatus/"
+      eligibility: "http://smartwork-dev-onboarding.idi.ntnu.no/onboarding/eligibility",
+      register: "http://smartwork-dev-onboarding.idi.ntnu.no/onboarding/patient",
+      status: "http://smartwork-dev-onboarding.idi.ntnu.no/onboarding/sendFromStatus/"
     }
   },
   defaults:{
