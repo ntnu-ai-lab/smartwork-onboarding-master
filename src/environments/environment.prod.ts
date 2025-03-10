8fe7b63a76046ff25d527205152270a2094cd7d4
anuja13 <anujagit@gmail.com>
2025-03-10 10:13:31 +0100

    endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   backend: {
     endpoints:{
-      eligibility: "https://smartwork-onboarding.idi.ntnu.no/onboarding/eligibility",
-      register: "https://smartwork-onboarding.idi.ntnu.no/onboarding/patient",
+      eligibility: "http://smartwork-dev-backend-user.idi.ntnu.no/onboarding/eligibility",
+       register: "http://smartwork-dev-backend-user.idi.ntnu.no/onboarding/patient",
       //status: "http://smartwork-onboarding.idi.ntnu.no/sendFromStatus/"
       //eligibility: "http://localhost:8013/onboarding/eligibility",
       //register: "http://localhost:8013/onboarding/patient",
